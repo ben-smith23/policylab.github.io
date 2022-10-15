559679d55bce3f778ac4e9a2fc7715cd70c14799
ben-smith23 <112433581+ben-smith23@users.noreply.github.com>
2022-10-15 11:15:01 -0700

    Update policy_scraper.py

--- Policy Lab/policy_scraper.py
+++ Policy Lab/policy_scraper.py
@@ -1,6 +1,6 @@
 from urllib.request import urlopen as Ureq
 from bs4 import BeautifulSoup as soup
 
-policyurl = "https://www.ncsl.org/research/elections-and-campaigns/elections-legislation-database.aspx"
+policyurl = "https://github.com/ben-smith23/policylab/raw/main/Policy%20Lab/enacted2020.html"
 
 enacted2020 = Ureq(policyurl)
